df1a858a73010e8f35d7ec746898d64b699c1f40
Barbare16 <sh.barbare@gmail.com>
2024-09-26 19:56:32 +0400

    commit

---
+++ level 03/Homework/level2.py
@@ -0,0 +1,13 @@
+'''შექმენით მინიმუმ 5 ცვლადი, რომლებშიც შეინახავთ თქვენი ოჯახის წევრების სახელებს (მაგალითად: mother: "Tamo"), შემდეგ კი დაბეჭდავთ ერთ დიდ წინადადებაში (მაგალითად print("my mother is" + " " + mother))'''
+
+Mother='Tamuna'
+Father='malkhazi'
+Grandpa='Nikolozi'
+Grandma='sofo'
+Aunt='Marina'
+
+print('My mother is ''Chokheli '+Mother)
+print('My father is ''Shaverdashvili '+Father)
+print('My grandpa is ''Chokheli '+Grandpa)
+print('My grandma is ''Kavtaradze '+Grandma)
+print('My aunt is ''Chokheli '+Aunt) 
