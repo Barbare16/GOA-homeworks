eea5609fcc7aa086cca44699c37a62b8b9497999
Barbare16 <sh.barbare@gmail.com>
2024-10-10 18:53:52 +0400

    commit

--- level 03/Homework/level2.py
+++ level 03/Homework/level2.py
@@ -6,8 +6,8 @@ Grandpa='Nikolozi'
 Grandma='sofo'
 Aunt='Marina'
 
-print('My mother is ''Chokheli '+Mother)
-print('My father is ''Shaverdashvili '+Father)
-print('My grandpa is ''Chokheli '+Grandpa)
-print('My grandma is ''Kavtaradze '+Grandma)
-print('My aunt is ''Chokheli '+Aunt) 
+print('My mother is Chokheli '+Mother)
+print('My father is Shaverdashvili '+Father)
+print('My grandpa is Chokheli '+Grandpa)
+print('My grandma is Kavtaradze '+Grandma)
+print('My aunt is Chokheli '+Aunt)
---
+++ level 04/homework/level4.py
@@ -0,0 +1,25 @@
+'შეასწორეთ (debug) შემდეგი კოდი:'
+
+name = ('Alice')
+print(name)
+
+salary = 75000
+
+# age -> 30
+age=30
+print(age)
+
+role = "Software Engineer"
+
+# print(role)
+print(role)
+
+'3) შექმენით 3 ცვლადი რომლებშიც შეინახავთ რიცხვით integer მნიშვნელობებს, შემდეგ სამივეს შორის დაბეჭდეთ შემდეგი ოპერაციების (+,-,*,/) შედეგები'
+Numb1 = 2
+Numb2 = 4
+Numb3 = 8
+
+print(Numb1+Numb2+Numb3)
+print(Numb3-Numb2-Numb1)
+print(Numb1*Numb2*Numb3)
+print(Numb3/Numb2/Numb1)
