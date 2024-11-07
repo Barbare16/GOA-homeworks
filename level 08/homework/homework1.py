869507616c91f8bfee77b897e294c27231e1b717
Barbare16 <sh.barbare@gmail.com>
2024-11-07 22:13:16 +0400

    'commit'

--- level 08/homework/homework1.py
+++ level 08/homework/homework1.py
@@ -10,4 +10,4 @@ age=input('enter your age: ')
 town=input('enter the name of the town where you live: ')
 
 #მთელი ეს ინფორმაცია ჩავსვი წინადადებებში
-print('Hi ' + first_name + ' ' + last_name + '!.  you are ' + age + ' years old. we will find english teacher for your age who will be able to teach you in ' + town + 'finding the teacher may take 3 days.' )
+print('Hi ' + first_name + ' ' + last_name + '!.  you are ' + age + ' years old. we will find english teacher for your age who will be able to teach you in ' + town + ' finding the teacher may take 3 days.' )
