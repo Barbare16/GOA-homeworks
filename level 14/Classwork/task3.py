07c6ee220c5f65184ac29af347701b1da112bce4
Barbare16 <sh.barbare@gmail.com>
2024-11-28 22:37:36 +0400

    'commit'

---
+++ level 14/Classwork/task3.py
@@ -0,0 +1,11 @@
+'''გააკეთეთ 2-2 მაგალითი ლოგიკურ ოპერატორებზე: and, or, not. კომენტარების საშვლაებით გვერდით მიუწერეთ შედეგი. ახსენით რა არის ლოგიკური ოპერატორი და როგორ მუშაობს ლოგიკური ოპერაცია'''
+print(True and False) # False
+print(False and False) # False
+
+print(True or False) # True
+print(True or True)
+
+
+
+
+
