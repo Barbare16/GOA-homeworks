51512ae13677e3d8b0b65e9aedf541c6b05b00f1
Barbare16 <sh.barbare@gmail.com>
2024-12-08 20:03:04 +0400

    'commit

---
+++ level 01/Homework/level01.py
@@ -0,0 +1,1 @@
+print('Barare Shaverdashvili')
--- level 14/Classwork/task3.py
+++ level 14/Classwork/task3.py
@@ -3,8 +3,12 @@ print(True and False) # False
 print(False and False) # False
 
 print(True or False) # True
-print(True or True)
+print(True or True) # True
 
+print(not False) # True
+print(not True) # False
+
+# ლოგიკური ოპერატორებია and, or და not. ისინი bool ტიპის მონაცემებს კრიბავენ და გამოაქვს ერთი აუთფუთი. როცა and-ს ვიყენებთ, თუ bool მონაცემებიდან ერთი მაინცაა false, პასუხია false, or ის დროს საკმარისია ერთი true ც და პასუხი true არის. ხოლო not ი მონაცემს აბრუნებს. მაგალითად: not True-False  not False - True
 
 
 
